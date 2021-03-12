1c2f47c27426d81b8c8906728a176a27525290a9
hikaru76 <eclipse.algorithm154176@gmail.com>
2021-03-13 01:41:27 +0900

    abc193 a finished

---
+++ abc193/A/main.py
@@ -0,0 +1,15 @@
+import bisect,collections,copy,heapq,itertools,math,numpy,string
+import sys
+def I(): return int(sys.stdin.readline().rstrip())
+def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
+def S(): return sys.stdin.readline().rstrip()
+def LS(): return list(sys.stdin.readline().rstrip().split())
+
+
+
+def main():
+	a,b = LI()
+	print(-(b-a)/a*100)
+
+if __name__ == "__main__":
+	main()
